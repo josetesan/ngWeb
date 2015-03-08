c11997cb965997e836a9842716d84e1d44ff53d7
Jose Luis Sanchez <muallin@gmail.com>
2015-03-08 21:14:36 +0100

    Idea basic AngularJS app

--- app/components/depot/depotController.js
+++
@@ -1,5 +0,0 @@
-var depotController = angular.module('depotController', []);
-
-depotController.controller('DepotListCtrl', ['$scope','Depot' function($scope,Depot){
-	$scope.depot = Depot.query();
-}])
--- app/components/depot/depotService.js
+++
@@ -1,7 +0,0 @@
-var depotServices = angular.module('depotServices', ['ngResource']);
-
-depotServices.factory('Depot', ['$resource',function($resource){
-    return $resource('depot/:userId.json', {}, {
-      query: {method:'GET', params:{userId:'userId'}, isArray:true}
-    });
-  }]);
